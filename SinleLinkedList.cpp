129221cc60d0cb1006e46e2d439e4dc56d01c4d2
mimyutt <mutiakhofifaturr@gmail.com>
2026-04-23 15:55:01 +0700

    menambahkan validasi duplikat di add node

--- SinleLinkedList.cpp
+++ SinleLinkedList.cpp
@@ -28,3 +28,14 @@ class LinkedList
 
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
+
+
+     
+
+     if (START == NULL || nim <= START->noMhs)
+     {
+        if (START != NULL && nim == START->noMhs)
+        {
+            cout << "/nDuplikasi noMhs tidak diijinkan\n";
+            return;
+        }
