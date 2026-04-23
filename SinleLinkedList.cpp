23f5a7f39f5a76c9c31399909d61fb93bc4375d9
mimyutt <mutiakhofifaturr@gmail.com>
2026-04-23 15:54:12 +0700

    menambahkan fungsi add node

--- SinleLinkedList.cpp
+++ SinleLinkedList.cpp
@@ -19,3 +19,12 @@ class LinkedList
      {
         START = NULL;
      }
+
+       void addNode()
+     {
+        int nim;
+        cout << "/nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
